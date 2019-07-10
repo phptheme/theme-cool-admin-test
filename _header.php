3213a3073329d3a238b1a6147950ab9bf139e8b1
phptheme <dev@getphptheme.com>
2019-07-10 17:57:22 +0300

    n/a

--- _header.php
+++ _header.php
@@ -1,5 +1,109 @@
 <?php
 
 $theme->beginLayout([
-    'title' => 'Title here...'
+    'mainMenu' => [
+        'items' => [
+            [
+                'label' => 'Settings',
+                'url' => '#',
+                'icon' => 'fa fa-wrench',
+                'items' => [
+                    [
+                        'icon' => 'fa fa-envelope',
+                        'label' => 'Messages',
+                        'url' => '#messages'
+                    ]
+                ]
+            ],
+            [
+                'label' => 'Pages',
+                'url' => '#',
+                'icon' => 'fa fa-file-text',
+                'active' => true
+            ],
+            [
+                'label' => 'Blocks',
+                'url' => '#',
+                'icon' => 'fa fa-th'
+            ]                        
+        ]
+    ],
+    'actionsMenu' => [
+        'items' => [
+            [
+                'label' => 'Create',
+                'url' => '#create',
+                'icon' => 'fa fa-plus',
+                'linkOptions' => [
+                    'class' => 'btn btn-success'
+                ]
+            ]
+        ]
+    ],
+    'breadcrumbs' => [
+        'items' => [
+            [
+                'label' => 'Home',
+                'url' => '#home'
+            ],
+            [
+                'label' => 'Users',
+                'url' => '#users'
+            ]            
+        ]
+    ],
+    'title' => 'Dashboard 3',
+    'optionsMenu' => [
+        'items' => [
+            [
+                'icon' => 'zmdi zmdi-account',
+                'label' => 'Account',
+                'url' => '#account'
+            ],
+            [
+                'label' => 'Settings',
+                'icon' => 'zmdi zmdi-settings',
+                'url' => '#settings'
+            ],
+            [
+                'label' => 'Billing',
+                'icon' => 'zmdi zmdi-money-box',
+                'url' => '#billing'
+            ]   
+        ]
+    ],
+    'notifications' => [
+        'items' => [
+            [
+                'label' => 'You got a email notification',
+                'date' => 'April 12, 2018 06:50',
+                'icon' => 'zmdi zmdi-file-text'
+            ]
+        ]
+    ],
+    'account' => [
+        'userName' => 'Denzel Po',
+        'userEmail' => 'denzel.po@example.com',
+        'logoutUrl' => '#logout',
+        'userUrl' => '#account',
+        'menu' => [
+            'items' => [
+                [
+                    'label' => 'Account',
+                    'icon' => 'zmdi zmdi-account',
+                    'url' => '#account'
+                ],
+                [
+                    'label' => 'Settings',
+                    'icon' => 'zmdi zmdi-settings',
+                    'url' => '#settings'
+                ],
+                [
+                    'label' => 'Billing',
+                    'icon' => 'zmdi zmdi-money-box',
+                    'url' => '#billing'
+                ]
+            ]
+        ]
+    ]
 ]);
--- index.php
+++ index.php
@@ -4,4 +4,26 @@ require __DIR__ . '/_common.php';
 
 require __DIR__ . '/_header.php';
 
+?>
+
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+<br>
+Hello 123 1 231 3123123
+
+<?php
+
 require __DIR__ . '/_footer.php';
